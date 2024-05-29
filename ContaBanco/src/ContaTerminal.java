eca2021fc45b292661388a6c17984a59b8c824a4
Rodolfo Suaki <rmsuaki@hotmail.com>
2024-05-28 22:28:45 -0300

    aplicando comandos para coletar dados do usuário ao interagir com a interface

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //Mensagem de entrada do sistema
+        System.out.println("Bem-vindo(a) ao cadastro de conta! \n");
+        
+        //Coletando a informação: Número.
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Por gentileza, digite o número da Agência.");
+        Integer numero = sc1.nextInt();
+
+        //Coletando a informação: Agência.
+        Scanner sc2 = new Scanner (System.in);
+        System.out.println("Por gentileza, digite a agência com o dígito.");
+        String agencia = sc2.nextLine();
+
+        //Coletando a informação: Nome do cliente.
+        Scanner sc3 = new Scanner (System.in);
+        System.out.println("Por gentileza, digite seu nome completo.");
+        String nome = sc3.nextLine();
+
+        //Coletando a informação: Saldo.
+        Scanner sc4 = new Scanner(System.in);
+        System.out.println("Por gentileza, digite seu saldo.");
+        Double saldo = sc4.nextDouble();
+        
+        //Imprimir o resultado
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
